3cf7bd068c1d18843fe714eb6e79f816ebf4d52d
pmgupta5 <pmgupta5@outlook.com>
2024-04-23 13:33:41 +0530

    updated latest code 22-Apr-2024

--- product-service/src/test/java/com/lti/productservice/CucumberIT.java
+++ product-service/src/test/java/com/lti/productservice/CucumberIT.java
@@ -1,28 +1,33 @@
 package com.lti.productservice;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com.lti.productservice")
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue= {"stepDefinitions"},
         plugin = {"pretty",
                 "html:target/report/cucumber.html",
-                "json:target/report/cucumber.json"
-        },
-        publish = true,
-        monochrome = true)
+                "json:target/report/cucumber.json",
+                "junit:target/report/Cucumber.xml"
+        }
+       )
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value="junit:target/cucumber-reports/Cucumber.xml," +
+                " json:target/cucumber-reports/Cucumber.json, " +
+                "html:target/cucumber-reports/Cucumber.html, " +
+                "timeline:target/cucumber-reports/CucumberTimeline")
 public class CucumberIT {
 
 }
